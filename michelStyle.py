b494ee43e20100857a9cbf655dc4ee88fc702bdb
Evan Jones <evan_jones@mac.com>
2020-03-16 13:49:32 -0500

    Refinements. Simplified program flow, did most calculations of which frames to style in `calculate_styling_params()` 

--- michelStyle.py
+++ michelStyle.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import sys
 import time
 
+# TODO: these imports take a long time. Import inline to decrease startup time? 
 import cv2
 import tensorflow as tf
 import numpy as np
@@ -16,32 +17,23 @@ import tensorflow_hub
 import moviepy
 import PIL
 
-from typing import List, Dict, Sequence, Tuple
-CVImage = np.ndarray # Mypy throws:  error: Variable "michelStyle.CVImageX" is not valid as a type
-                     # TODO: figure out how to get around it
-# ===========
-# = GLOBALS =
-# ===========
-IMAGE_SUFFIXES = ('.jpg', '.jpeg', '.png', '.gif')
-MAXBATCH = 100
-
-
-
-TF_HUB = None
-def get_tf_hub():
-    global TF_HUB
-    if not TF_HUB:
-        TF_HUB = tensorflow_hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-    return TF_HUB
+# ==============
+# = TYPE HINTS =
+# ==============
+from typing import List, Dict, Sequence, Tuple, Any
+class CVImage(np.ndarray): pass 
 
 def main():
     args = parse_all_args()
-    style_video(source_dir_or_image=args.source, styles_dir=args.styles, output_dir=args.frames)
-    print(f'Wrote frames to {args.frames}')
+    total_elapsed = style_video(source_dir_or_image=args.source, styles_dir=args.styles, output_dir=args.output_frames)    
+    print(f'Wrote frames to {args.output_frames}')
 
-    if not args.no_video:
-        output_path = write_video_file(args.frames, output_path=args.output, 
+    if args.video:
+        vid_start = time.time()
+        output_path = write_video_file(args.frames, output_path=args.video, 
                         fps=args.fps, audio_path=args.audio)
+        total_elapsed += (time.time() - vid_start)
+    print(f'Total time: {total_elapsed/60:.1f} minutes')
 
 def parse_all_args(args_in=None):
     ''' Set up argparser and return a namespace with named
@@ -49,54 +41,92 @@ def parse_all_args(args_in=None):
     If help is requested (-h / --help) the help message will be printed 
     and the program will exit.
     '''
-    program_description = '''Output a video with styles transferred onto each image'''
+    program_description = '''Output a video with styles transferred onto each frame'''
 
     parser = argparse.ArgumentParser( description=program_description,
-                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+                formatter_class=argparse.HelpFormatter)
 
     # Replace these with your arguments below
-    parser.add_argument( '--styles', type=Path, required=True,
-        help=('A directory containing image files to take styles from. Each '
-        'image should have a number for the frame it should be most applied to '))
     parser.add_argument('--source', type=Path, required=True,
         help=('A directory containing frames of a movie sequence that should have '
         'styles applied to them. Frame numbers start at 1. A single image may also '
         'be supplied'))
+
+    parser.add_argument( '--styles', type=Path, required=True,
+        help=('A directory containing image files to take styles from. Each '
+        'image should have a number for the frame it should be most applied to '))
+
+    parser.add_argument('-o','--output_frames', type=Path, default=Path('styled_frames'),
+        help='Path to an output directory where stylized frames will be written.  Default: "%(default)s"')
     
-    parser.add_argument('-o', '--output', type=Path, default=Path('video_out.mp4'),
-        help='Path to output .mp4 to. Default: %(default)s')
-    parser.add_argument('--frames', type=Path, default=Path('stylized_frames'),
-        help='Path to a directory where stylized frames will be written. ')
-    parser.add_argument('--no_video', action='store_true', default=False,
-        help='Don\'t write a video file, just stylized frames to --OUTPUT_FRAMES')
-    parser.add_argument('-f', '--fps', type=int, default=24,
-        help='Destination frame rate. Default: %(default)s')
+    parser.add_argument('-v', '--video', type=Path, 
+        help='Path to an MP4 output file.')
+
     parser.add_argument('-a', '--audio', type=Path, default=None,
         help='Path to an  MP3 file. If specified, it will be added to the '
         'generated video')
-    parser.add_argument('--force_overwrite', action='store_true', default=False,
-        help=('If specified, force already-written files in the OUTPUT_FRAMES directory '
-        'to be overwritten' ))
+
+    parser.add_argument('-f', '--fps', type=int, default=24,
+        help='Destination frame rate. Default: %(default)s')
+
+    # TODO: support this option
+    # parser.add_argument('--force_overwrite', action='store_true', default=False,
+    #     help=('If specified, force already-written files in the OUTPUT_FRAMES directory '
+    #     'to be overwritten' ))
+
+    # print help if no args are supplied
+    if len(sys.argv) <= 2: 
+        sys.argv.append('--help')
 
     # If args_in isn't specified, args will be taken from sys.argv
     args = parser.parse_args(args_in)
 
     # Validation:
-    # if not args.source.exists()
+    if not args.source.exists():
+        raise 
+
+    ensure_dir(args.output_frames)
 
     return args
 
 def style_video(source_dir_or_image: Path, 
                 styles_dir: Path, 
-                output_dir:Path=None) -> bool:
-    
+                output_dir:Path) -> float:
+    total_start_time = time.time()
+    params = calculate_styling_params(source_dir_or_image, styles_dir)
+    print('Transferring styles...\n\n')
     hub_module = get_tf_hub()
 
+    frame_count = len(params)
+    style_images: Dict[Path, CVImage] = {}
+
+    single_source_file = is_image(source_dir_or_image)
+    if single_source_file:
+        source_image = frame_image(source_dir_or_image, as_float=True)
+
+    for frame, (source_path, style_a_path, style_b_path, style_ratio) in params.items():
+        start_time = time.time()
+        output_path = output_dir / f'{frame}.jpg'
+
+        if not single_source_file:
+            source_image = frame_image(source_path, as_float=True)
+
+        style_a_image = style_images.setdefault(style_a_path, frame_image(style_a_path))
+        style_b_image = style_images.setdefault(style_b_path, frame_image(style_a_path))
+
+        stylized_image = transfer_styles(source_image, style_a_image, style_b_image, style_ratio, hub_module)
+        stylized_image.save(output_path)
+
+        infoscreen(frame, frame_count, time.time() - start_time)
+
+    return time.time() - total_start_time
+
+def calculate_styling_params(source_dir_or_image: Path, 
+                             styles_dir: Path,) -> Dict[int, Tuple[Path, Path, Path, float]]:
+    params: Dict[int, Tuple[Path, Path, Path, float]] = {}
+
     # Figure out how many frames we'll need
-    if source_dir_or_image.is_file(): # assume source_dir_or_image is a single image file
-        source_frame_paths = {1: source_dir_or_image}
-    else:
-        source_frame_paths = numbered_images_dict(source_dir_or_image)
+    source_frame_paths = numbered_images_dict(source_dir_or_image)
     style_frame_paths = numbered_images_dict(styles_dir)
 
     style_frame_numbers = sorted(style_frame_paths.keys())
@@ -107,12 +137,13 @@ def style_video(source_dir_or_image: Path,
 
     style_args: Dict[int, Tuple[Path, Path, float]]= {}
 
-    # If we have fewer frames than we do styles what do we do? 
-    # This is what Remi originally did, with a single frame looping through
-    # several styles
     # TODO: get frame lengths from movies, too. 
     # TODO: Handle missing source frames, e.g. 1.jpg & 3.jpg exist, but not 2.jpg
-    frame_count = max(last_source_frame, last_style_frame)
+    frame_count = last_style_frame
+
+    if len(source_frame_numbers) == 1:
+        source_path = source_frame_paths[first_source_frame]
+        source_frame_paths = dict({f: source_path for f in range(1,last_style_frame + 1)})
 
     # Insert beginning and end elements in the style transitions so the 
     # entire frame range is covered by a pair of style images
@@ -123,62 +154,21 @@ def style_video(source_dir_or_image: Path,
         style_frame_paths[frame_count] = style_frame_paths[last_style_frame]
 
     style_transitions = sorted(list(style_frame_paths.keys()))
-    transition_pairs = list(zip(style_transitions[:-1], style_transitions[1:]))
+    transition_pairs = zip(style_transitions[:-1], style_transitions[1:])
 
-    # Make sure we have styles for the initial and final frames
-    # TODO: we want something like writing an 0.jpg or <large_n>.jpg
-    # with the contents of the first/last style frames
-    which_transition = 0
-    start_frame, end_frame = transition_pairs[which_transition]
-    style_start_image = frame_image(style_frame_paths[start_frame])
-    style_end_image = frame_image(style_frame_paths[end_frame])
+    for start_frame, end_frame in transition_pairs:
+        style_a_path = style_frame_paths[start_frame]
+        style_b_path = style_frame_paths[end_frame]
 
-    style_end_image: CVImage
-    style_ratio: float
-    stylized_image: CVImage  
+        for frame in range(start_frame, end_frame + 1):
+            # if frame == start_frame, we will have just calculated its params
+            # for the previous start_frame/end_frame pair; skip this step
+            if frame in params:
+                continue
+            style_ratio = (frame - start_frame)/(end_frame - start_frame)
+            params[frame] = (source_frame_paths[frame], style_a_path, style_b_path, style_ratio)
 
-    single_source_frame = (len(source_frame_numbers) == 1)
-    if single_source_frame:
-        source_image = frame_image(source_frame_paths[first_source_frame], as_float=True)
-
-    for i in range(1, frame_count + 1):
-
-        start_time = time.time()
-        start_frame, end_frame = transition_pairs[which_transition]
-
-        # Only load style images from disk when we change the pairs of style images
-        # if i == end_frame and which_transition < len(transition_pairs) - 1:
-        if i > end_frame:
-            which_transition += 1
-            start_frame, end_frame = transition_pairs[which_transition]
-            style_start_image = frame_image(style_frame_paths[start_frame])
-            style_end_image   = frame_image(style_frame_paths[end_frame])
-        
-        style_ratio = (i - start_frame)/(end_frame - start_frame)
-
-        # FIXME: We currently assume we have either: 
-        # a) more source frames than styles, or 
-        # b) a single source frame. 
-        # TODO: object if style frames have a larger value than the number
-        # of source frames
-        # If there are multiple source frames, but more style frames, we'd 
-        # be scaling the original video in some problematic ways, so skip that for now
-        if not single_source_frame:
-            source_image = frame_image(source_frame_paths[i], as_float=True)
-
-        # TODO: Currently, we don't check whether this image might already have
-        # been written. That could speed up development, if for example, you
-        # were repeatedly running this program to sync up style changes with
-        # music, say. -ETJ 13 March 2020
-        stylized_image = transfer_styles(source_image, style_start_image, style_end_image, style_ratio, hub_module)
-        output_path = output_dir / f'{i}.jpg'
-        stylized_image.save(output_path)
-
-        frames_to_go = frame_count - i
-        estimated_minutes_remaining = ((time.time() - start_time) * frames_to_go)/60
-        infoscreen(frames_to_go, estimated_minutes_remaining)
-
-    return True
+    return params
 
 def transfer_styles(source_image_as_floats:CVImage, 
                     style_a:CVImage, 
@@ -241,6 +231,19 @@ def write_video_file(frames_dir: Path, output_path: Path=None, fps=24, audio_pat
 # ===========
 # = HELPERS =
 # ===========
+def get_tf_hub():
+    TF_HUB = tensorflow_hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+    # importing hub_module prints some debug info to my screen. Remove that
+    clear_screen()
+    return TF_HUB
+
+def tensor_to_image(tensor):
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor)>3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
 
 def ensure_dir(dir_name:Path) -> bool:
     if dir_name.exists():
@@ -252,38 +255,50 @@ def ensure_dir(dir_name:Path) -> bool:
         dir_name.mkdir()
         return True
 
-def clear():
+def clear_screen():
     if sys.platform == 'darwin':
         os.system('clear')
     else: 
         os.system('cls')
 
-def tensor_to_image(tensor):
-    tensor = tensor*255
-    tensor = np.array(tensor, dtype=np.uint8)
-    if np.ndim(tensor)>3:
-        assert tensor.shape[0] == 1
-        tensor = tensor[0]
-    return PIL.Image.fromarray(tensor)
+def infoscreen(frame: int, total_frames:int, frame_elapsed: float):
+    minutes_left = (frame_elapsed * (total_frames - frame))/60
 
-def infoscreen(remaining_frames: int, minutes_left: float):
-    clear()
-    print("--------------------------")
-    print("  MichelStyle - Remideza  ")
-    print("--------------------------")
+    line = f"   SimpleStyleTransfer - {frame}/{total_frames}"
+    marquee = '='*(len(line) + 3)
+
+    clear_screen()
+    print(marquee)
+    print(line)
+    print(marquee)
     print()
-    print(f"{remaining_frames} remaining frames to work on")
+    # print(f"------------------------------------")
+    # print(f"   SimpleStyleTransfer - {frame}/{total_frames}")
+    # print(f"------------------------------------")
+    print()
     print(f"{minutes_left:.1f} minutes remaining")
 
 def is_image(image:Path) -> bool:
+    IMAGE_SUFFIXES = ('.jpg', '.jpeg', '.png', '.gif')
+
     return image.exists() and image.is_file() and image.suffix in IMAGE_SUFFIXES
 
 def numbered_images_dict(a_dir: Path) -> Dict[int, Path]:
     result: Dict[int, Path] = {}
-    for f in a_dir.iterdir():
-        # TODO: maybe accept files with a number anyplace in the stem?
-        if f.stem.isdigit() and is_image(f):
-            result[int(f.stem)] = f
+
+    # If a_dir is an image file, not a directory, we'll just return a single 
+    # element dict
+    if is_image(a_dir):
+        result = {1: a_dir}
+    elif a_dir.is_dir():
+        for f in a_dir.iterdir():
+            # TODO: maybe accept files with a number anyplace in the stem?
+            if f.stem.isdigit() and is_image(f):
+                result[int(f.stem)] = f
+    else:
+        raise ValueError(f'argument {a_dir} is neither a directory nor an image '
+                        'file we know how to handle')
+                
     return result
 
 def frame_image(path:Path, as_float:bool = False) -> CVImage: 
@@ -291,6 +306,7 @@ def frame_image(path:Path, as_float:bool = False) -> CVImage:
     if as_float:
         img = img.astype(np.float32)[np.newaxis, ...] / 255.0
     return img
+
 
 if __name__ == '__main__':
     main()
